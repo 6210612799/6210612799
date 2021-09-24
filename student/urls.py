63648d64d61c53ff2cf015b1b2d204234bff56dc
Prinkarn Ditsomboon <6210612781@cn103>
2021-09-25 02:45:58 +0700

    update student

--- student/urls.py
+++ student/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('realuser/', include('realuser.urls')),
-    
+    path('', include('realuser.urls')),
 ]
  
