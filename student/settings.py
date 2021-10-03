310abc3659de3536b0e0924828d3e477c946d32c
Prinkarn Ditsomboon <6210612781@cn103>
2021-10-03 19:10:08 +0700

    update setting

--- student/settings.py
+++ student/settings.py
@@ -132,5 +132,5 @@ STATICFILES_DIRS = ( os.path.join('static'), )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
